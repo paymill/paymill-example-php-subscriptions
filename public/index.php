725c5002ba3f612a32eb5eaf81fc294b9160a93d
BSc Vassil Nikolov <vassil.nikolov@qaiware.com>
2013-12-12 03:34:06 +0200

    change use definition

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use LlamaKisses\ClassLoader\ApplicationLoader;
+use \LlamaKisses\ClassLoader\ApplicationLoader;
 
 $twigLoader = new Twig_Loader_Filesystem( '../src/Paymill/LlamaKisses/Views/' );
 $twig = new Twig_Environment( $twigLoader, array( 'cache' => './cache', 'debug' => true ) );

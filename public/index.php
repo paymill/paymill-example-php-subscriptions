0d883972bd0c6cd99374e596df003f11e97dcd5c
BSc Vassil Nikolov <vassil.nikolov@qaiware.com>
2013-12-12 03:36:15 +0200

    remove use from index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use \LlamaKisses\ClassLoader\ApplicationLoader;
-
 $twigLoader = new Twig_Loader_Filesystem( '../src/Paymill/LlamaKisses/Views/' );
 $twig = new Twig_Environment( $twigLoader, array( 'cache' => './cache', 'debug' => true ) );
 
 //create the controller and execute the action
-$appLoader = new ApplicationLoader($_GET);
+$appLoader = new \LlamaKisses\ClassLoader\ApplicationLoader($_GET);
 $controller = $appLoader->CreateController($twig);
 $controller->ExecuteAction();

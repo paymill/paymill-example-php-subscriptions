557f66431701b426fa0552a1f5d277a729678d79
BSc Vassil Nikolov <vassil.nikolov@qaiware.com>
2013-12-25 01:19:43 +0200

    [trigger:composer]

--- index.php
+++ index.php
@@ -12,13 +12,11 @@ $link = create_database( $log );
 seed_database( $log, $link );
 mysqli_close( $link );
 
-use LlamaKisses\ClassLoader\ApplicationLoader;
-
 $twigLoader = new Twig_Loader_Filesystem( 'src/Paymill/LlamaKisses/Views/' );
 $twig = new Twig_Environment( $twigLoader, array( 'cache' => 'cache', 'debug' => true ) );
 
 //create the controller and execute the action
-$appLoader = new ApplicationLoader($_GET);
+$appLoader = new \LlamaKisses\ClassLoader\ApplicationLoader($_GET);
 $controller = $appLoader->CreateController($twig);
 $controller->ExecuteAction();
 
